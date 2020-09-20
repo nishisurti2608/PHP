8f36f16bbdecfdf00eb0f8b313faefa367f54936
nishisurti2608 <Nsurti2191@conestogac.on.ca>
2020-09-20 14:16:21 -0400

    Setting

--- NishiSurti_Assignment-1.php
+++ NishiSurti_Assignment-1.php
@@ -1,2 +1,18 @@
-<? php
+<?php
+
+
+ $case = 14.33; 
+ $height = 12.26; 
+ $area = 0.5*($case*$height); 
+ $result = round($area,2); 
+ echo"<p> The area of triangle is ". $result. "</p>"; 
+
+$MY_NAME = 'Nishi Surti';
+$MY_NAME = strtoupper($MY_NAME);
+echo "My first name is <strong>".$MY_NAME."</strong>"
+
+
+
 ?>
+
+
